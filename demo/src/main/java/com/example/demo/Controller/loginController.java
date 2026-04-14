bc54ab05ecb6526519d9a7b9ca4cc96d58ff62b3
diegocartajena-2022510@kinal.edu.gt <dcartajena-2022510@kinal.edu.gt>
2026-04-14 11:51:54 -0600

    delete:se elimino clase extra

--- demo/src/main/java/com/example/demo/Controller/loginController.java
+++ demo/src/main/java/com/example/demo/Controller/loginController.java
@@ -1,5 +1,7 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.UsuariosService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,44 +12,5 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class loginController {
-
-    @GetMapping("/")
-    public String inicio() {return "redirect:/login";}
-
-    @GetMapping("/login")
-    public String mostrarLogin(){return "login";}
-
-    @PostMapping("/login")
-    public String login(@RequestParam String usuario,
-                        @RequestParam String password,
-                        HttpSession session,
-                        Model model){
-        String userCorrecto="admin";
-        String passCorrecto="1234";
-
-        if (usuario.equals(userCorrecto)&& password.equals(passCorrecto)){
-            // guarda sesion
-            session.setAttribute("usuarioLogeado",usuario);
-            return "redirect:/principal";
-        }else{
-            model.addAttribute("error","usuario y contraseña incorrectas");
-            return "login";
-        }
-    }
-
-    @GetMapping("/principal")
-    public String mostrarPrincipal(HttpSession session){
-        // valida la sesion
-        if (session.getAttribute("usuarioLogeado") == null) {
-            return "redirect:/login";
-        }
-        return "principal";
-    }
-
-
-    @GetMapping("/register")
-    public String register() {return "register";}
-    
-
 
 }
