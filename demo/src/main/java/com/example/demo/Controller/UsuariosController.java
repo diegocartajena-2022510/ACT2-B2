405d47b2979792e14f969b4b2a2efe95498513da
diegocartajena-2022510@kinal.edu.gt <dcartajena-2022510@kinal.edu.gt>
2026-04-14 11:51:14 -0600

    feat: se agrego metodos de login y eliminar

--- demo/src/main/java/com/example/demo/Controller/UsuariosController.java
+++ demo/src/main/java/com/example/demo/Controller/UsuariosController.java
@@ -3,73 +3,66 @@ package com.example.demo.Controller;
 
 import com.example.demo.Entity.Usuarios;
 import com.example.demo.Service.UsuariosService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-@RestController
-@RequestMapping("/api/Usuarios")
+@Controller
 public class UsuariosController {
-    private final UsuariosService usuariosService;
 
-    public UsuariosController(UsuariosService usuariosService) {
-        this.usuariosService = usuariosService;
-    }
+    @Autowired
+    private UsuariosService service;
 
-    @GetMapping
-    public List<Usuarios> getAllUsuarios(){return usuariosService.getAllUsuarios();}
+    @GetMapping("/")
+    public String inicio() {return "redirect:/login";}
 
-    @PostMapping
-    public ResponseEntity<Object> createUsuarios(@Valid @RequestBody Usuarios usuarios){
-        try{
-            Usuarios createdUsuarios= usuariosService.saveUsuarios(usuarios);
-            return new ResponseEntity<>(createdUsuarios, HttpStatus.CREATED);
+    @GetMapping("/login")
+    public String mostrarLogin(){return "login";}
 
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @PostMapping("/login")
+    public String login(@RequestParam String usuario,
+                        @RequestParam String password,
+                        HttpSession session,
+                        Model model){
+        Usuarios u = service.login(usuario, password);
+        if (u != null) {
+            session.setAttribute("usuarioLogeado", u);
+            return "redirect:/principal";
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas");
+            return "login";
         }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateUsuarios(@PathVariable Integer id, @Valid @RequestBody Usuarios usuarios) {
-        try{
-            Usuarios usuarios1 = usuariosService.getUsuariosById(id);
-            if (usuarios1 == null) {
-                return ResponseEntity.notFound().build();
-            }
-            usuarios.setCodigo_usuario(id);
+    @GetMapping("/register")
+    public String register() {return "register";}
 
-            Usuarios updateUsuarios = usuariosService.updateUsuarios(id, usuarios);
-            return ResponseEntity.ok(updateUsuarios);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @PostMapping("/register")
+    public String guardar(@RequestParam String usuario,
+                          @RequestParam String password,
+                          @RequestParam String email,
+                          Model model) {
+
+        Usuarios u = service.registrar(usuario, password,email);
+
+        if (u == null) {
+            model.addAttribute("error", "Usuario ya existe");
+            return "register";
         }
+        return "redirect:/login";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteUsuarios(@PathVariable Integer id) {
-        try {
 
-            Usuarios usuarios = usuariosService.getUsuariosById(id);
-
-            usuariosService.deleteUsuarios(id);
-            return ResponseEntity.noContent().build();
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @GetMapping("/principal")
+    public String mostrarPrincipal(HttpSession session){
+        // valida la sesion
+        if (session.getAttribute("usuarioLogeado") == null) {
+            return "redirect:/login";
         }
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getUsuariosById(@PathVariable Integer id) {
-        try {
-            Usuarios usuarios= usuariosService.getUsuariosById(id);
-            return ResponseEntity.ok(usuarios);
-
-        }catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        return "principal";
     }
 }
+
