35827b9ea6fa96044c4eb8e9594f18dc9dcb2851
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-04-14 21:34:54 -0600

    feat:se agrego metodo de cruds

--- demo/src/main/java/com/example/demo/Controller/UsuariosController.java
+++ demo/src/main/java/com/example/demo/Controller/UsuariosController.java
@@ -1,15 +1,18 @@
 package com.example.demo.Controller;
 
 
-import com.example.demo.Entity.Usuarios;
-import com.example.demo.Service.UsuariosService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.example.demo.Entity.Usuarios;
+import com.example.demo.Service.UsuariosService;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class UsuariosController {
 
@@ -64,5 +67,15 @@ public class UsuariosController {
         }
         return "principal";
     }
+
+    
+    @GetMapping("/usuarios") 
+    public String mostrarUsuarios(Model model) {
+        model.addAttribute("usuarios", service.listar());
+        
+        return "usuarios"; 
+    }
+    
+    
 }
 
